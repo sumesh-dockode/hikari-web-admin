b20ca882c5e6ddd598f736f0009b8542511a47ef
shifna1 <shifna@dockode.com>
2025-03-27 11:40:55 +0530

    mingler updated

--- apps/isomorphic/src/app/shared/account-settings/navigation.tsx
+++ apps/isomorphic/src/app/shared/account-settings/navigation.tsx
@@ -22,22 +22,22 @@ const menuItems = [
     label: 'Password',
     value: '/forms/profile-settings/password',
   },
-  {
-    label: 'Team',
-    value: '/forms/profile-settings/team',
-  },
-  {
-    label: 'Billing',
-    value: '/forms/profile-settings/billing',
-  },
-  {
-    label: 'Notifications',
-    value: '/forms/profile-settings/notification',
-  },
-  {
-    label: 'Integrations',
-    value: '/forms/profile-settings/integration',
-  },
+  // {
+  //   label: 'Team',
+  //   value: '/forms/profile-settings/team',
+  // },
+  // {
+  //   label: 'Billing',
+  //   value: '/forms/profile-settings/billing',
+  // },
+  // {
+  //   label: 'Notifications',
+  //   value: '/forms/profile-settings/notification',
+  // },
+  // {
+  //   label: 'Integrations',
+  //   value: '/forms/profile-settings/integration',
+  // },
 ];
 
 export default function ProfileSettingsNav() {

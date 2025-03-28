8a82d5a02e929ec45a847976eddbc9f5b4a69572
Jithin <jithin@dockode.com>
2025-03-28 10:35:19 +0530

    Installed react query | set access token in session while login. | created a api.ts file to store API urls

--- apps/isomorphic/src/hooks/usePaginatedCategories.tsx
+++ apps/isomorphic/src/hooks/usePaginatedCategories.tsx
@@ -1,9 +1,44 @@
-"use client"
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { useSession } from 'next-auth/react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { API_ROUTES } from '@/api';
 
 export default function usePaginatedCategories() {
-  return (
-    <div>usePaginatedCategories</div>
-  )
-}
+  const { data: session, status } = useSession();
 
+  const fetchCategories = async (pageParam: number = 1) => {
+    let url = `${API_ROUTES.categories}?page=${pageParam}`;
+    try {
+      const res = await fetch(url, {
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${session?.user?.accessToken}`,
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error('Failed to fetch categories');
+      }
+
+      const data = await res.json();
+      console.log('Fetched Categories:', data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+    }
+  };
+
+  return useInfiniteQuery({
+    queryKey: ['categories'],
+    queryFn: ({ pageParam = 1 }) => fetchCategories(pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages, lastPageParam) => {
+      // let last = lastPage ? (lastPage as any).next : null;
+      return lastPage ? lastPageParam + 1 : null;
+      // return lastPage.next ? lastPageParam + 1 : null;
+    },
+    enabled: status === 'authenticated',
+  });
+}

d871d638e4ec67cd03f57766009513538433bee5
Jithin <jithin@dockode.com>
2025-04-23 14:01:40 +0530

    order listing | order details page | order status change api integration

--- apps/isomorphic/src/app/providers.tsx
+++ apps/isomorphic/src/app/providers.tsx
@@ -1,11 +1,24 @@
 'use client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       staleTime: 10 * 1000, // 10 second
+      retry: (failureCount, error) => {
+        const err = error as AxiosError;
+        console.log('err', err);
+        console.log('error', error);
+        if (
+          err?.response !== undefined &&
+          [403, 404, 503, 504].includes(err?.response?.status)
+        ) {
+          return false;
+        }
+        return failureCount < 2;
+      },
     },
   },
 });

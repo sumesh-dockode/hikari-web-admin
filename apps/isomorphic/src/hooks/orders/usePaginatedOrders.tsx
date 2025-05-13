d2029863ab8914b8601c5ed8df84e805e8a409e1
Jithin <jithin@dockode.com>
2025-05-13 14:04:46 +0530

    Order detailed API integration | Order cancel button implemented | status stepper change for cancelled order. | .split error fixed when opening order detail page

--- apps/isomorphic/src/hooks/orders/usePaginatedOrders.tsx
+++ apps/isomorphic/src/hooks/orders/usePaginatedOrders.tsx
@@ -1,30 +1,44 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { API_ROUTES } from '@/app/lib/api';
 import apiClient from '@/app/lib/apiClient';
 
 export default function usePaginatedOrders(options: {
   pageIndex: number;
   pageSize: number;
+  search?: string;
 }) {
   const { status } = useSession();
 
   const fetchOrders = async () => {
-    let url = `${API_ROUTES.orders}?page=${options.pageIndex + 1}&page_size=${options.pageSize}`;
+    let url = `${API_ROUTES.orders}`;
+    let params = [];
+
+    if (options?.pageIndex || options?.pageIndex === 0) {
+      params.push(`page=${options.pageIndex + 1}`);
+    }
+    if (options?.pageSize) {
+      params.push(`page_size=${options.pageSize}`);
+    }
+    if (options?.search) {
+      params.push(`search=${options.search}`);
+    }
+    if (params.length > 0) {
+      url += `?${params.join('&')}`;
+    }
+
     const { data } = await apiClient.get(url);
 
     return data;
   };
-  return useInfiniteQuery({
+  return useQuery({
     queryKey: ['ordersTable', options],
     queryFn: fetchOrders,
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages) => {
-      if (!lastPage?.data?.next) return undefined;
-      return allPages.length + 1;
-    },
     enabled: status === 'authenticated',
+    throwOnError(error, query) {
+      throw error;
+    },
   });
 }

fceda22c8cdf947ff7da3589881dc445b1886f05
Shamnaek <141479896+Shamnaek@users.noreply.github.com>
2025-06-19 09:05:39 +0530

    commit

--- apps/isomorphic/src/app/shared/ecommerce/servicebooking/service-detail-gallery.tsx
+++ apps/isomorphic/src/app/shared/ecommerce/servicebooking/service-detail-gallery.tsx
@@ -147,16 +147,24 @@ export default function ServiceDetailsGallery({
                     className="block text-sm font-medium text-gray-700"
                     htmlFor="serviceTo"
                   >
-                    Service To
+                    Product Name
                   </label>
-                  <input
+                  {/* <input
                     type="text"
                     id="serviceTo"
                     name="serviceTo"
                     value={currentService?.service_to || 'N/A'}
                     readOnly
                     className="mt-1 block w-full rounded-md border border-gray-300 text-gray-900 shadow-sm focus:outline-none sm:text-sm"
-                  />
+                  /> */}
+                  <input
+                      type="text"
+                      id="serviceTo"
+                      name="serviceTo"
+                      value={currentService?.product?.name || currentService?.service_to || 'N/A'}
+                      readOnly
+                      className="mt-1 block w-full rounded-md border border-gray-300 text-gray-900 shadow-sm focus:outline-none sm:text-sm"
+                    />
                 </div>
 
                 <div>
